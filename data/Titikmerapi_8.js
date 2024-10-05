21e7714cca5ec6e1e5ca39e49574a7d6bb1440a9
Mahardika Yurico Billian <mahardikayurico.my@gmail.com>
2024-10-05 21:44:29 +0700

    Add files via upload

---
+++ data/Titikmerapi_8.js
@@ -0,0 +1,1 @@
+var json_Titikmerapi_8 = {"type":"FeatureCollection","name":"Titikmerapi_8","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"Point","coordinates":[110.446207096834542,-7.541277091547172]}}]}
